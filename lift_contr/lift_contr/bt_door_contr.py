a546d904c9639b78f9d524d9e110c0c236e5d5d9
josefgst <josefgstoettner@live.at>
2023-05-04 10:25:49 +0800

    make it a class

--- lift_contr/lift_contr/bt_door_contr.py
+++ lift_contr/lift_contr/bt_door_contr.py
@@ -1,8 +1,7 @@
 import asyncio
 from bleak import BleakClient
+from time import sleep
 
-OPEN_DOOR = 1
-CLOSE_DOOR = 0
 
 # DOOR
 TKO_DOOR_wanted_name = "TKO_DOOR"
@@ -13,21 +12,35 @@ MODEL_NBR_UUID_door = "34860001-e699-4650-ae12-f1f3c8bf9ad9"
 address_lift = "F5:EC:6E:A0:BF:CD"
 MODEL_NBR_UUID_lift = "34860001-E699-4650-ae12-f1f3c8bf9ad9"
 
-
-async def door(cmd, address, uuid):
-    client = BleakClient(address)
-    try:
-        await client.connect()
-        write_value = bytearray([cmd])
-        await client.write_gatt_char(uuid, write_value)
-        print("success")
-    except Exception as e:
-        print(e)
-    finally:
-        await client.disconnect()
+class BtDoor:
+    def __init__(self, adr, UUID):
+        self.address = adr
+        self.uuid = UUID
+        self.OPEN = 1
+        self.CLOSE = 0
+        
+        
+    async def door(self, cmd):
+        client = BleakClient(self.address)
+        try:
+            await client.connect()
+            write_value = bytearray([cmd])
+            await client.write_gatt_char(self.uuid, write_value)
+            print("success")
+        except Exception as e:
+            print(e)
+        finally:
+            await client.disconnect()
 
 if __name__ == "__main__":
-    asyncio.run(door(OPEN_DOOR,address_lift, MODEL_NBR_UUID_lift))
-    print("finished")
-    asyncio.run(door(CLOSE_DOOR,address_lift, MODEL_NBR_UUID_lift))
-    print("finished")
+    lift = BtDoor(address_lift,MODEL_NBR_UUID_lift)
+    asyncio.run(lift.door(lift.OPEN))
+    print("open")
+    asyncio.run(lift.door(lift.CLOSE))
+    sleep(1)
+    print("close")
+    
+    # asyncio.run(door(OPEN_DOOR,address_lift, MODEL_NBR_UUID_lift))
+    # print("finished")
+    # asyncio.run(door(CLOSE_DOOR,address_lift, MODEL_NBR_UUID_lift))
+    # print("finished")
