d01bab1f99e79d50f725bbef3a25653062208139
josefgst <josefgstoettner@live.at>
2023-05-23 17:49:53 +0800

    read coordinates from yaml

---
+++ deliverybot_mqtt/launch/example_launch.py
@@ -0,0 +1,11 @@
+from ament_index_python.packages import get_package_share_path
+import launch
+import launch_ros.actions
+
+
+def generate_launch_description():
+    return launch.LaunchDescription([
+        launch_ros.actions.Node(name='mqtt_node', package='deliverybot_mqtt', executable='mqtt_pub',
+                                parameters=[
+                                    str(get_package_share_path('deliverybot_mqtt') / 'config/example.yaml')]),
+    ])
--- deliverybot_mqtt/src/mqtt_pub.cpp
+++ deliverybot_mqtt/src/mqtt_pub.cpp
@@ -1,6 +1,7 @@
 #include <memory>
 #include <chrono>
 #include <functional>
+#include <string.h>
 
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/pose_with_covariance_stamped.hpp"
@@ -18,6 +19,16 @@ public:
     MqttNode()
         : Node("mqtt_node")
     {
+        this->declare_parameter("map", rclcpp::PARAMETER_STRING);
+        this->declare_parameter("robot_id", rclcpp::PARAMETER_INTEGER);
+        this->declare_parameter("num_of_locations", rclcpp::PARAMETER_INTEGER);
+        this->declare_parameter("location.location_id", rclcpp::PARAMETER_INTEGER_ARRAY);
+        this->declare_parameter("location.location_name", rclcpp::PARAMETER_STRING_ARRAY);
+        this->declare_parameter("location.coordinate_x", rclcpp::PARAMETER_DOUBLE_ARRAY);
+        this->declare_parameter("location.coordinate_y", rclcpp::PARAMETER_DOUBLE_ARRAY);
+        this->declare_parameter("location.orientation_z", rclcpp::PARAMETER_DOUBLE_ARRAY);
+        this->declare_parameter("location.orientation_w", rclcpp::PARAMETER_DOUBLE_ARRAY);
+
         status_pub_ = this->create_publisher<deliverybot_mqtt::msg::Status>("robots/status", 10);
         location_pub_ = this->create_publisher<deliverybot_mqtt::msg::Location>("robots/location", 10);
         config_pub_ = this->create_publisher<deliverybot_mqtt::msg::Configuration>("robots/configuration", 10);
@@ -31,31 +42,43 @@ public:
 private:
     void location_callback(const geometry_msgs::msg::PoseWithCovarianceStamped::SharedPtr msg) const
     {
+        rclcpp::Parameter map = this->get_parameter("map");
         // Location publisher
         auto location = deliverybot_mqtt::msg::Location();
         location.pose = msg->pose.pose;
-        location.map = "testmap";
+        location.map = map.as_string();
         location_pub_->publish(location);
     }
 
     void timer_callback()
     {
+        rclcpp::Parameter robot_id = this->get_parameter("robot_id");
+        rclcpp::Parameter num_of_locations = this->get_parameter("num_of_locations");
+        rclcpp::Parameter location_id = this->get_parameter("location.location_id");
+        rclcpp::Parameter location_name = this->get_parameter("location.location_name");
+        rclcpp::Parameter coordinate_x = this->get_parameter("location.coordinate_x");
+        rclcpp::Parameter coordinate_y = this->get_parameter("location.coordinate_y");
+        rclcpp::Parameter orientation_w = this->get_parameter("location.orientation_z");
+        rclcpp::Parameter orientation_z = this->get_parameter("location.orientation_w");
         // Configuration publisher
         auto config = deliverybot_mqtt::msg::Configuration();
         auto location_entry = deliverybot_mqtt::msg::LocationEntry();
-        config.robot_id = 1;
-        for (int i = 0; i < 2; i++)
+        config.robot_id = robot_id.as_int();
+        for (int i = 0; i < num_of_locations.as_int(); i++)
         {
-            location_entry.id = i;
-            location_entry.name = "A";
-            location_entry.pose.position.x = i;
+            location_entry.id = location_id.as_integer_array()[i];
+            location_entry.name = location_name.as_string_array()[i];
+            location_entry.pose.position.x = coordinate_x.as_double_array()[i];
+            location_entry.pose.position.y = coordinate_y.as_double_array()[i];
+            location_entry.pose.orientation.z = orientation_z.as_double_array()[i];
+            location_entry.pose.orientation.w = orientation_w.as_double_array()[i];
             config.location.push_back(location_entry);
         }
         config_pub_->publish(config);
 
         // Status publisher
         auto status = deliverybot_mqtt::msg::Status();
-        status.id = 1;
+        status.id = robot_id.as_int();
         status.status = "PROCESSING";
         status.battery_remaining = 56;
         status.from_location_id = 1;
