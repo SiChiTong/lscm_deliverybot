2cf7019c108888337e51d948f895f0ea9d9b8a4b
josefgst <josefgstoettner@live.at>
2023-05-25 17:47:43 +0800

    finish string json pub

---
+++ deliverybot_mqtt/launch/example_string_launch.py
@@ -0,0 +1,20 @@
+from ament_index_python.packages import get_package_share_path
+import launch
+import launch_ros.actions
+
+
+def generate_launch_description():
+    return launch.LaunchDescription([
+        launch_ros.actions.Node(name='mqtt_node', package='deliverybot_mqtt', executable='mqtt_pub_string',
+                                parameters=[
+                                    str(get_package_share_path('deliverybot_mqtt') / 'config/example.yaml')]),
+        launch_ros.actions.Node(name='mqtt_configuration', package='mqtt_client', executable='mqtt_client',
+                                parameters=[
+                                    str(get_package_share_path('deliverybot_mqtt') / 'config/params.ros2.yaml')]),
+        launch_ros.actions.Node(name='mqtt_status', package='mqtt_client', executable='mqtt_client',
+                                parameters=[
+                                    str(get_package_share_path('deliverybot_mqtt') / 'config/params.ros2.yaml')]),
+        launch_ros.actions.Node(name='mqtt_location', package='mqtt_client', executable='mqtt_client',
+                                parameters=[
+                                    str(get_package_share_path('deliverybot_mqtt') / 'config/params.ros2.yaml')]),
+    ])
---
+++ deliverybot_mqtt/src/mqtt_pub_string.cpp
@@ -0,0 +1,129 @@
+#include <memory>
+#include <chrono>
+#include <functional>
+#include <string.h>
+
+#include "rclcpp/rclcpp.hpp"
+#include "geometry_msgs/msg/pose_with_covariance_stamped.hpp"
+#include "std_msgs/msg/string.hpp"
+// #include "deliverybot_mqtt_interfaces/msg/status.hpp"
+#include "deliverybot_mqtt_interfaces/msg/location.hpp"
+#include "deliverybot_mqtt_interfaces/msg/location_entry.hpp"
+// #include "deliverybot_mqtt_interfaces/msg/configuration.hpp"
+
+using std::placeholders::_1;
+using namespace std::chrono_literals;
+
+class MqttNode : public rclcpp::Node
+{
+public:
+    MqttNode()
+        : Node("mqtt_node")
+    {
+        // declare params
+        this->declare_parameter("map", rclcpp::PARAMETER_STRING);
+        this->declare_parameter("robot_id", rclcpp::PARAMETER_INTEGER);
+        this->declare_parameter("num_of_locations", rclcpp::PARAMETER_INTEGER);
+        this->declare_parameter("location.location_id", rclcpp::PARAMETER_INTEGER_ARRAY);
+        this->declare_parameter("location.location_name", rclcpp::PARAMETER_STRING_ARRAY);
+        this->declare_parameter("location.coordinate_x", rclcpp::PARAMETER_DOUBLE_ARRAY);
+        this->declare_parameter("location.coordinate_y", rclcpp::PARAMETER_DOUBLE_ARRAY);
+        this->declare_parameter("location.orientation_z", rclcpp::PARAMETER_DOUBLE_ARRAY);
+        this->declare_parameter("location.orientation_w", rclcpp::PARAMETER_DOUBLE_ARRAY);
+
+        rclcpp::Parameter robot_id = this->get_parameter("robot_id");
+        status_pub_ = this->create_publisher<std_msgs::msg::String>("robots/id_" + std::to_string(robot_id.as_int()) + "/status", 10);
+        location_pub_ = this->create_publisher<std_msgs::msg::String>("robots/id_" + std::to_string(robot_id.as_int()) + "/location", 10);
+        config_pub_ = this->create_publisher<std_msgs::msg::String>("robots/configuration", 10);
+
+        timer_ = this->create_wall_timer(1000ms, std::bind(&MqttNode::timer_callback, this));
+
+        subscription_ = this->create_subscription<geometry_msgs::msg::PoseWithCovarianceStamped>(
+            "amcl_pose", 10, std::bind(&MqttNode::location_callback, this, _1));
+    }
+
+private:
+    double robot_pose[7] = {};
+
+    void location_callback(const geometry_msgs::msg::PoseWithCovarianceStamped::SharedPtr msg)
+    {
+        // save location in local array
+        robot_pose[0] = msg->pose.pose.position.x;
+        robot_pose[1] = msg->pose.pose.position.y;
+        robot_pose[5] = msg->pose.pose.orientation.z;
+        robot_pose[6] = msg->pose.pose.orientation.w;
+
+        // location_pub_->publish(location);
+    }
+
+    void timer_callback()
+    {
+        rclcpp::Parameter robot_id = this->get_parameter("robot_id");
+        rclcpp::Parameter num_of_locations = this->get_parameter("num_of_locations");
+        rclcpp::Parameter location_id = this->get_parameter("location.location_id");
+        rclcpp::Parameter location_name = this->get_parameter("location.location_name");
+        rclcpp::Parameter coordinate_x = this->get_parameter("location.coordinate_x");
+        rclcpp::Parameter coordinate_y = this->get_parameter("location.coordinate_y");
+        rclcpp::Parameter orientation_w = this->get_parameter("location.orientation_z");
+        rclcpp::Parameter orientation_z = this->get_parameter("location.orientation_w");
+        // Configuration publisher
+        auto config_ros = std_msgs::msg::String();
+        std::string config = "{\"robot_id\":" + std::to_string(robot_id.as_int());
+        auto location_entry = std_msgs::msg::String();
+        config.append(",\"location\":[");
+        for (int i = 0; i < num_of_locations.as_int(); i++)
+        {
+            config.append("{\"id\":" + std::to_string(location_id.as_integer_array()[i]));
+            config.append(",\"name\":\"" + location_name.as_string_array()[i] + "\"");
+            config.append(",\"coordinate\":[" + std::to_string(coordinate_x.as_double_array()[i]) + ",");
+            config.append(std::to_string(coordinate_y.as_double_array()[i]) + ",");
+            config.append("0.000000]");
+            config.append(",\"orientation\":[0.000000,0.000000,");
+            config.append(std::to_string(orientation_w.as_double_array()[i]) + ",");
+            config.append(std::to_string(orientation_z.as_double_array()[i]) + "]");
+            config.append("},");
+        }
+        config.pop_back(); // remove the last character ","
+        config.append("]}");
+        config_ros.data = config;
+        config_pub_->publish(config_ros);
+
+        // Status publisher
+        auto status_ros = std_msgs::msg::String();
+        std::string status = "{\"robot_id\":" + std::to_string(robot_id.as_int());
+        status.append(",\"status\":\"PROCESSING\"");
+        status.append(",\"battery_remaining\":56");
+        status.append(",\"from_location_id\":1");
+        status.append(",\"to_location_id\":2");
+        status.append("}");
+        status_ros.data = status;
+        status_pub_->publish(status_ros);
+
+        // Location publisher
+        rclcpp::Parameter map = this->get_parameter("map");
+        auto location_ros = std_msgs::msg::String();
+        std::string location = "{\"location\":{\"coordinate\":[" + std::to_string(robot_pose[0]);
+        location.append("," + std::to_string(robot_pose[1]));
+        location.append(",0.000000],\"orientation\":[0.000000,0.000000");
+        location.append("," + std::to_string(robot_pose[5]));
+        location.append("," + std::to_string(robot_pose[6]));
+        location.append("]},\"map\":\"" + map.as_string() + "\"}");
+        location_ros.data = location;
+        location_pub_->publish(location_ros);
+    }
+    rclcpp::TimerBase::SharedPtr timer_;
+
+    rclcpp::Subscription<geometry_msgs::msg::PoseWithCovarianceStamped>::SharedPtr subscription_;
+
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr status_pub_;
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr location_pub_;
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr config_pub_;
+};
+
+int main(int argc, char *argv[])
+{
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<MqttNode>());
+    rclcpp::shutdown();
+    return 0;
+}
