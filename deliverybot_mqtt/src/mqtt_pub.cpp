678a59cc2342e3ced4bd0aebf619277ae599a98a
josefgst <josefgstoettner@live.at>
2023-05-25 10:55:17 +0800

    separate mqtt msg into separate pkg

--- deliverybot_mqtt/src/mqtt_pub.cpp
+++ deliverybot_mqtt/src/mqtt_pub.cpp
@@ -5,10 +5,10 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/pose_with_covariance_stamped.hpp"
-#include "deliverybot_mqtt/msg/status.hpp"
-#include "deliverybot_mqtt/msg/location.hpp"
-#include "deliverybot_mqtt/msg/location_entry.hpp"
-#include "deliverybot_mqtt/msg/configuration.hpp"
+#include "deliverybot_mqtt_interfaces/msg/status.hpp"
+#include "deliverybot_mqtt_interfaces/msg/location.hpp"
+#include "deliverybot_mqtt_interfaces/msg/location_entry.hpp"
+#include "deliverybot_mqtt_interfaces/msg/configuration.hpp"
 
 using std::placeholders::_1;
 using namespace std::chrono_literals;
@@ -31,9 +31,9 @@ public:
         this->declare_parameter("location.orientation_w", rclcpp::PARAMETER_DOUBLE_ARRAY);
 
         rclcpp::Parameter robot_id = this->get_parameter("robot_id");
-        status_pub_ = this->create_publisher<deliverybot_mqtt::msg::Status>("robots/id_" + std::to_string(robot_id.as_int()) + "/status", 10);
-        location_pub_ = this->create_publisher<deliverybot_mqtt::msg::Location>("robots/id_" + std::to_string(robot_id.as_int()) + "/location", 10);
-        config_pub_ = this->create_publisher<deliverybot_mqtt::msg::Configuration>("robots/configuration", 10);
+        status_pub_ = this->create_publisher<deliverybot_mqtt_interfaces::msg::Status>("robots/id_" + std::to_string(robot_id.as_int()) + "/status", 10);
+        location_pub_ = this->create_publisher<deliverybot_mqtt_interfaces::msg::Location>("robots/id_" + std::to_string(robot_id.as_int()) + "/location", 10);
+        config_pub_ = this->create_publisher<deliverybot_mqtt_interfaces::msg::Configuration>("robots/configuration", 10);
 
         rclcpp::Parameter num_of_locations = this->get_parameter("num_of_locations");
         rclcpp::Parameter location_id = this->get_parameter("location.location_id");
@@ -43,8 +43,8 @@ public:
         rclcpp::Parameter orientation_w = this->get_parameter("location.orientation_z");
         rclcpp::Parameter orientation_z = this->get_parameter("location.orientation_w");
         // Configuration publisher
-        auto config = deliverybot_mqtt::msg::Configuration();
-        auto location_entry = deliverybot_mqtt::msg::LocationEntry();
+        auto config = deliverybot_mqtt_interfaces::msg::Configuration();
+        auto location_entry = deliverybot_mqtt_interfaces::msg::LocationEntry();
         config.robot_id = robot_id.as_int();
         for (int i = 0; i < num_of_locations.as_int(); i++)
         {
@@ -69,7 +69,7 @@ private:
     {
         rclcpp::Parameter map = this->get_parameter("map");
         // Location publisher
-        auto location = deliverybot_mqtt::msg::Location();
+        auto location = deliverybot_mqtt_interfaces::msg::Location();
         location.pose = msg->pose.pose;
         location.map = map.as_string();
         location_pub_->publish(location);
@@ -86,8 +86,8 @@ private:
         // rclcpp::Parameter orientation_w = this->get_parameter("location.orientation_z");
         // rclcpp::Parameter orientation_z = this->get_parameter("location.orientation_w");
         // // Configuration publisher
-        // auto config = deliverybot_mqtt::msg::Configuration();
-        // auto location_entry = deliverybot_mqtt::msg::LocationEntry();
+        // auto config = deliverybot_mqtt_interfaces::msg::Configuration();
+        // auto location_entry = deliverybot_mqtt_interfaces::msg::LocationEntry();
         // config.robot_id = robot_id.as_int();
         // for (int i = 0; i < num_of_locations.as_int(); i++)
         // {
@@ -102,7 +102,7 @@ private:
         // config_pub_->publish(config);
 
         // Status publisher
-        auto status = deliverybot_mqtt::msg::Status();
+        auto status = deliverybot_mqtt_interfaces::msg::Status();
         status.robot_id = robot_id.as_int();
         status.status = "PROCESSING";
         status.battery_remaining = 56;
@@ -114,9 +114,9 @@ private:
 
     rclcpp::Subscription<geometry_msgs::msg::PoseWithCovarianceStamped>::SharedPtr subscription_;
 
-    rclcpp::Publisher<deliverybot_mqtt::msg::Status>::SharedPtr status_pub_;
-    rclcpp::Publisher<deliverybot_mqtt::msg::Location>::SharedPtr location_pub_;
-    rclcpp::Publisher<deliverybot_mqtt::msg::Configuration>::SharedPtr config_pub_;
+    rclcpp::Publisher<deliverybot_mqtt_interfaces::msg::Status>::SharedPtr status_pub_;
+    rclcpp::Publisher<deliverybot_mqtt_interfaces::msg::Location>::SharedPtr location_pub_;
+    rclcpp::Publisher<deliverybot_mqtt_interfaces::msg::Configuration>::SharedPtr config_pub_;
 };
 
 int main(int argc, char *argv[])
