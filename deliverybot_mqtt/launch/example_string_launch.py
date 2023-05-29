a061216df5a5a4f3b865b8a5c9dfdf1fc1d4fd7c
josefgst <josefgstoettner@live.at>
2023-05-29 15:20:17 +0800

    remove mqtt2ros in params

--- deliverybot_mqtt/launch/example_string_launch.py
+++ deliverybot_mqtt/launch/example_string_launch.py
@@ -8,6 +8,7 @@ def generate_launch_description():
         launch_ros.actions.Node(name='mqtt_node', package='deliverybot_mqtt', executable='mqtt_pub_string',
                                 parameters=[
                                     str(get_package_share_path('deliverybot_mqtt') / 'config/example.yaml')]),
+                                    
         launch_ros.actions.Node(name='mqtt_configuration', package='mqtt_client', executable='mqtt_client',
                                 parameters=[
                                     str(get_package_share_path('deliverybot_mqtt') / 'config/params.ros2.yaml')]),
